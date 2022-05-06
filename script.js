aebd67d128112b454a4913a6051976b9dcfa8953
Ruan S <76751124+thisRuan@users.noreply.github.com>
2022-05-06 12:02:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ function openPage(pageName,elmnt,color) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+  window.onscroll = function() {myFunction()};
+
+  window.onscroll = function() {myFunction()};
